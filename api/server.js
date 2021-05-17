293cf81ce3c97e49101828c699202b583cbdad92
Francis Nguyen <francishtknguyen@gmail.com>
2021-05-17 16:06:15 -0500

    node-auth1 completed middleware functions

--- api/server.js
+++ api/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -14,18 +19,43 @@ const cors = require("cors");
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
  */
+const sessionConfig = {
+  name: "chocolatechip",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // if true the cookie is not set unless it's an https connection
+    httpOnly: false, // if true the cookie is not accessible through document.cookie
+  },
+  rolling: true,
+  resave: false, // some data stores need this set to true
+  saveUninitialized: false, // privacy implications, if false no cookie is set on client unless the req.session is changed
+  // ALLOWS TO SAVE THE SESSIONS TO DB
+  store: new KnexSessionStore({
+    knex: require("../database/db-config.js"), // configured instance of knex
+    tablename: "sessions", // table that will store sessions inside the db, name it anything you want
+    sidfieldname: "sid", // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  }),
+};
 
 const server = express();
 
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
